bc47992149fc8e6434636b0cd8bb2aa26791d8b6
xbhknbst6g-jpg <xbhknbst6g@privaterelay.appleid.com>
2026-07-29 18:37:32 +0300

    Update prices.js

--- prices.js
+++ prices.js
@@ -1,5 +1,5 @@
 // URL-адрес вашего сервера, где запущен бот и API
-const API_URL = "/api/v1/tariffs";
+const API_URL = "http://144.124.241.83:8080/api/v1/tariffs";
 
 // 🟢 1. ДЕФОЛТНЫЕ ЗАГЛУШКИ ДЛЯ КАРТИНОК (РИСОВАНИЯ)
 window.dbFluxDevPrices = { coins_min: 1, coins_mid: 2, coins_max: 3 };
